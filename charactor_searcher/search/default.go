e90d101143982636c9702861c93a15197c6d6485
weiwei3.wang <weiwei3.wang@ly.com>
2019-04-22 19:48:53 +0800

    完成匹配器核心代码

--- charactor_searcher/search/default.go
+++ charactor_searcher/search/default.go
@@ -1,5 +1,7 @@
 package search
 
+import "log"
+
 // defaultMatcher 实现了默认的匹配器
 type defaultMatcher struct{}
 
@@ -8,6 +10,16 @@ func init() {
 	// 空结构体在创建实例时，不会分配任何内存
 	var matcher defaultMatcher
 	Register("default", matcher)
+}
+
+// Register 调用时，会注册一个匹配器，提供给后面的程序使用
+// 匹配器的注册器在main函数调用之前调用
+func Register(feedType string, matcher Matcher) {
+	if _, exists := matchers[feedType]; exists {
+		log.Fatalln(feedType, "Matcher already registered")
+	}
+	log.Println("Register ", feedType, " matcher")
+	matchers[feedType] = matcher
 }
 
 func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error) {
--- charactor_searcher/search/match.go
+++ charactor_searcher/search/match.go
@@ -1,5 +1,7 @@
 package search
 
+import "log"
+
 /**
 以一致且通用的方法，来处理不同类型的匹配器值
 */
@@ -15,4 +17,29 @@ type Result struct {
 // 如果接口类型内部声明了多个方法，其名字需要与其行为关联
 type Matcher interface {
 	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+// match 函数，为每个数据源单独启动 goroutine 来执行这个函数并发执行搜索
+// 这里的通道是一个单向通道，只允许发送数据
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan<- *Result) {
+	// 对特定的匹配器执行搜索
+	searchResults, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// 将结果写入通道
+	for _, result := range searchResults {
+		results <- result
+	}
+}
+
+//Display 从每个单独的 goroutine 接受到结果后，在终端窗口输出
+func Display(results chan *Result) {
+	// 通道会一直阻塞，直到有数据写入
+	// 一旦通道被关闭，for循环就会终止
+	for result := range results {
+		log.Printf("%s:\n%s\n\n", result.Field, result.Content)
+	}
 }
